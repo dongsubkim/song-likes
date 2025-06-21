6da72206d61c84a9fbae3611a1d010613a5d9d7f
dongsubkim <dskim9218@gmail.com>
2025-06-22 01:33:03 +0900

    add domain module

---
+++ domain/src/main/java/com/example/songlikes/domain/SongLikesDomainConfiguration.java
@@ -0,0 +1,9 @@
+package com.example.songlikes.domain;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = {"com.example.songlikes.domain"})
+public class SongLikesDomainConfiguration {
+}
