44619aa430365b108d99b81ce7d92bcab73b61d0
dongsubkim <dskim9218@gmail.com>
2025-06-25 18:11:40 +0900

    add Test configs

---
+++ domain/src/test/java/com/example/songlikes/domain/SongLikeDomainIntegrationTestConfiguration.java
@@ -0,0 +1,8 @@
+package com.example.songlikes.domain;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// domain 모듈의 Project Files에는 SpringBootApplication이 없어서 SpringBootTest가 작동하기 위해서는 Test Files 에서 SpringBootApplication이 필요합니다.
+@SpringBootApplication
+public class SongLikeDomainIntegrationTestConfiguration {
+}
