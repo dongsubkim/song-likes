22d899bb94f72ebe18a12cef1f42df266eaf480c
dongsubkim <dskim9218@gmail.com>
2025-06-26 15:30:35 +0900

    add hourly top 10 api

--- api/src/test/java/com/example/songlikes/api/controller/SongControllerTest.java
+++ api/src/test/java/com/example/songlikes/api/controller/SongControllerTest.java
@@ -1,11 +1,13 @@
 package com.example.songlikes.api.controller;
 
 import com.example.songlikes.api.service.SongLikeService;
+import com.example.songlikes.domain.dto.TopLikedSongDto;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import static org.mockito.Mockito.*;
@@ -35,5 +37,33 @@ public class SongControllerTest {
 
         // verify service call
         verify(songLikeService, times(1)).likeSong(songId);
+    }
+
+    @Test
+    void getTop10LikedSongsInLastHour_success() {
+        // given
+        when(songLikeService.findTop10LikedSongInLastHour()).thenReturn(Flux.just(
+            new TopLikedSongDto(10L, 10L),
+            new TopLikedSongDto(9L, 9L),
+            new TopLikedSongDto(8L, 8L),
+            new TopLikedSongDto(7L, 7L),
+            new TopLikedSongDto(6L, 6L),
+            new TopLikedSongDto(5L, 5L),
+            new TopLikedSongDto(4L, 4L),
+            new TopLikedSongDto(3L, 3L),
+            new TopLikedSongDto(2L, 2L),
+            new TopLikedSongDto(1L, 1L)
+        ));
+
+        // when
+        webTestClient.get()
+            .uri("/api/songs/likes/hourly-top-10")
+            .exchange()
+            .expectStatus().isOk()
+            .expectBodyList(TopLikedSongDto.class)
+            .hasSize(10);
+
+        // verify service call
+        verify(songLikeService, times(1)).findTop10LikedSongInLastHour();
     }
 }
